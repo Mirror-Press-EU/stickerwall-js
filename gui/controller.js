bda5d8c1044a9473882486030e723192c51c717c
Mirror-Press-EU <96450758+Mirror-Press-EU@users.noreply.github.com>
2022-02-14 23:54:37 +0100

    GUI - Controller Static Init
    createPinNotice( 100, 100, "test", "test" );

--- gui/controller.js
+++ gui/controller.js
@@ -5,7 +5,15 @@ export default class Controller {
     this.stickerWall = new StickerWallManager( );
 
     // Default "Empty here" Splash Screen
+
+    // Test Init
+    this.createPinNotice( 100, 100, "test", "test-title", "test text... test text... test text... test text... " );
+    //this.stickerWall.loadFolderFromLocalStorage( );
   }
+
+  /*| _____________
+ --*| --- Storage ---
+ --*/
 
   loadFolderFromLocalStorage( ) {
     let localStorageJsonStr = localStorage.getItem( 'folder' );
@@ -18,6 +26,78 @@ export default class Controller {
           this.stickerWall.loadFromJSON( localStorageJson );
         }
       }
+    }
+  }
+
+
+  /*| ____________________
+ --*| --- EVENT HANDLE ---
+ --*/
+
+  bindButtonsEvents( ) {
+
+    // --- GUI-Module ---
+    this.bindZoomBar( );
+    this.bindToolBox( );
+
+    // --- Buttons ---
+    this.bindSaveButton( )
+
+    // --- Open Dialogs ---
+    this.bindCreateQouteButton( );
+    this.bindCreateConnection( );
+  }
+
+  bindZoomBar( ) {
+    let zoomBarControllMapping = this.getElement( [ 'sidebar', 'zoom' ] );
+    
+    if (zoomBarControllMapping) this.setElement(
+      [ 'sidebar', 'zoom', 'instance' ],
+      new CostumZoomBar( zoomBarControllMapping, this.canDrawer )
+    );
+  }
+
+  bindSaveButton( ) {
+    let canDisplay = this.getElement( [ 'canvasDisplay' ] );
+    let saveFolderButton = this.getElement( [ 'saveFolderButton' ] );
+
+    if (saveFolderButton && canDisplay) {
+      saveFolderButton.root.addEventListener(
+        'click', _=> {
+          saveFolderButton.root.classList.remove( "changed" );
+          localStorage.setItem( 'folder', this.exportToJSON( ) );
+        }
+      );
+      canDisplay.addEventListener(
+        'click', _=> saveFolderButton.root.classList.add( "changed" )
+      );
+    }
+  }
+
+  bindToolBox( ) {
+    let openToolBoxButton = this.getElement( [ 'toolbox', 'openButton' ] );
+    
+    if (openToolBoxButton) openToolBoxButton.root.addEventListener(
+      "click", ( evt ) => this.onOpenUtilityListClicked( evt )
+    );
+  }
+
+  bindCreateQouteButton( ) {
+    let createQouteButton = this.getElement( [ 'toolbox', 'createButtons', 'qoute' ] );
+    let modQouteDialog = this.getElement([ 'dialogs', 'modifyPinQoute', 'container' ]);
+
+    if (modQouteDialog && createQouteButton) createQouteButton.root.addEventListener(
+      "click", ( evt ) => this.onCreateQouteButtonClicked( evt )
+    );
+  }
+
+  bindCreateConnection( ) {
+    let createConnectionButton = this.getElement( [ 'toolbox', 'createButtons', 'connection' ] );
+
+    if (createConnectionButton
+    &&  this.pinFolder.getPinCount( ) > 0
+    ) {
+      this.pinFolder.onEditorModeChanged( "onEditorModeChanged", "ATTACHING", this ); // Wechsel alle Pins in den Attaching Mode
     }
   }
 }
